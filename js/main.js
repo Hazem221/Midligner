5ca904ba54573d8a52eed70947e8241660cc9023
Hazem <hazem@waffarha.com>
2023-09-18 09:21:04 +0300

    complete slider

--- js/main.js
+++ js/main.js
@@ -1,46 +1,12 @@
-// const scrollToTop = document.getElementById("scroll-to-top");
-// scrollToTop.addEventListener("click", () => {
-//   window.scrollTo({
-//     top: 0,
-//     behavior: "smooth",
-//   });
-// });
-
 $(document).ready(function () {
   $(".main-slider").slick({
     infinite: false,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true,
     dots: true,
-    speed: 300,
-    // responsive: [
-    //   {
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 3,
-    //       infinite: true,
-    //       dots: false,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 600,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 2,
-    //       dots: false,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //       dots: false,
-    //     },
-    //   },
-    // ],
+    arrows: true,
+    prevArrow: $(".main-prev-arrow"),
+    nextArrow: $(".main-next-arrow"),
   });
 
   $(document).ready(function () {
@@ -52,9 +18,6 @@ $(document).ready(function () {
       arrows: true,
       prevArrow: $(".prev-arrow"),
       nextArrow: $(".next-arrow"),
-      // centerMode: true,
-      // variableWidth: true,
-      // rtl: false,
     });
   });
 });
